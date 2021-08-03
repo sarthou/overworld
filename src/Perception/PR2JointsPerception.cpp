1a35d8ca62771f1e8d25f832f6e333c139149f6a
guilhembn <gbuisan@laas.fr>
2021-08-03 10:38:11 +0200

    [PR2Joints] Updates robot pose

--- src/Perception/PR2JointsPerception.cpp
+++ src/Perception/PR2JointsPerception.cpp
@@ -3,9 +3,9 @@
 namespace owds {
 
 PR2JointsPerception::PR2JointsPerception(ros::NodeHandle* n, int robot_body_id, std::vector<std::string> link_to_entity_names,
-                                         BulletClient* robot_world_client)
+                                         BulletClient* robot_world_client, double min_period)
     : PerceptionModuleRosBase(n, "/joint_states"), robot_body_id_(robot_body_id), link_to_entity_names_(link_to_entity_names),
-      bullet_(robot_world_client)
+      tf2_listener_(tf_buffer_), bullet_(robot_world_client), min_period_(min_period)
 {
     auto p = bullet_->findJointAndLinkIndices(robot_body_id_);
     joint_name_id_ = p.first;
@@ -13,15 +13,29 @@ PR2JointsPerception::PR2JointsPerception(ros::NodeHandle* n, int robot_body_id, 
     for (const auto& name : link_to_entity_names_)
     {
         percepts_.emplace(name, name);
-        if (link_name_id_.count(name) == 0){
-            std::cout << "Error: link name '" << name << "' passed as 'link_to_entity_names' of ctor of PR2JointsPerception does not exist in Bullet.";
-            throw std::runtime_error("Link name not found in Bullet.");
+        if (link_name_id_.count(name) == 0)
+        {
+            std::cout << "Error: link name '" << name
+                      << "' passed as 'link_to_entity_names' of ctor of PR2JointsPerception does not exist in Bullet.";
+            throw std::runtime_error("Link name '" + name + "' not found in Bullet.");
         }
     }
+    percepts_.emplace("base_footprint", "base_footprint");
 }
 
 bool PR2JointsPerception::perceptionCallback(const sensor_msgs::JointState& msg)
 {
+    if ((ros::Time::now() - last_update_).toSec() < min_period_){
+        return false;
+    }
+    geometry_msgs::TransformStamped robot_base = tf_buffer_.lookupTransform("map", "base_footprint", msg.header.stamp, ros::Duration(1.0));
+    bullet_->resetBasePositionAndOrientation(
+        robot_body_id_, {robot_base.transform.translation.x, robot_base.transform.translation.y, robot_base.transform.translation.z},
+        {robot_base.transform.rotation.x, robot_base.transform.rotation.y, robot_base.transform.rotation.z, robot_base.transform.rotation.w});
+    percepts_.at("base_footprint")
+        .updatePose(
+            {robot_base.transform.translation.x, robot_base.transform.translation.y, robot_base.transform.translation.z},
+            {robot_base.transform.rotation.x, robot_base.transform.rotation.y, robot_base.transform.rotation.z, robot_base.transform.rotation.w});
     for (size_t i = 0; i < msg.name.size(); i++)
     {
         std::string name = msg.name[i];
@@ -39,6 +53,7 @@ bool PR2JointsPerception::perceptionCallback(const sensor_msgs::JointState& msg)
         double* rot = link.m_worldLinkFrameOrientation;
         percepts_.at(name).updatePose({{pos[0], pos[1], pos[2]}}, {{rot[0], rot[1], rot[2], rot[3]}}, msg.header.stamp);
     }
+    last_update_ = ros::Time::now();
     return true;
 }
 } // namespace owds
