7ca49c6a0d1ad553fd65654786cd7561dcbafeab
Guillaume Sarthou <gsarthou@laas.fr>
2021-08-04 09:09:16 +0200

    [overworld_node] add SIGGEGV handler

--- src/Nodes/overworld.cpp
+++ src/Nodes/overworld.cpp
@@ -1,7 +1,26 @@
 #include <overworld/SituationAssessor.h>
 
+#include <stdio.h>
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+void handler(int sig)
+{
+  void *array[10];
+  size_t size;
+
+  size = backtrace(array, 10);
+
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 int main(int argc, char** argv)
 {
+  signal(SIGSEGV, handler);
   ros::init(argc, argv, "listener");
 
   owds::SituationAssessor robot_situation_assessor("pr2_robot", true);
