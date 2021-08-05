e3cd67e71424e9c971dc2e79806405b9f8b998d5
Guillaume Sarthou <gsarthou@laas.fr>
2021-08-05 15:43:31 +0200

    [ADD] OntologeniusFactsPublisher

---
+++ src/Facts/Publisher/OntologeniusFactsPublisher.cpp
@@ -0,0 +1,25 @@
+#include "overworld/Facts/Publisher/OntologeniusFactsPublisher.h"
+
+namespace owds {
+
+OntologeniusFactsPublisher::OntologeniusFactsPublisher(ros::NodeHandle* n,
+                                                       const std::string& agent_name) : agent_name_(agent_name),
+                                                                                        ontologies_manipulator_(n)
+{
+  ontologies_manipulator_.waitInit();
+  ontologies_manipulator_.add("robot");
+  onto_ = ontologies_manipulator_.get("robot");
+  onto_->close();
+}
+
+void OntologeniusFactsPublisher::addToKb(const Fact& fact)
+{
+  onto_->feeder.addProperty(fact.getSubject(), fact.getPredicate(), fact.getObject());
+}
+
+void OntologeniusFactsPublisher::removeFromKb(const Fact& fact)
+{
+  onto_->feeder.removeProperty(fact.getSubject(), fact.getPredicate(), fact.getObject());
+}
+  
+}
