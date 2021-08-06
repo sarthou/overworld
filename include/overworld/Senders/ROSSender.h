e62d38e165270fe0df42837ffb33640802b4a6d2
Guillaume Sarthou <gsarthou@laas.fr>
2021-08-06 08:53:48 +0200

    [ROSSender] use template parameters and pointers to Entity

--- include/overworld/Senders/ROSSender.h
+++ include/overworld/Senders/ROSSender.h
@@ -2,7 +2,7 @@
 #define OWDS_ROSSENDER_H
 
 #include "SharedMemory/PhysicsClientC_API.h"
-#include "overworld/BasicTypes/Object.h"
+#include "overworld/BasicTypes/Entity.h"
 #include <visualization_msgs/MarkerArray.h>
 
 #include <geometry_msgs/TransformStamped.h>
@@ -18,12 +18,12 @@ class ROSSender
   public:
     ROSSender(ros::NodeHandle* n);
 
-    void sendEntitiesToTF(const std::vector<Entity>& entities);
-    void sendEntitiesToRViz(const std::string& topic_name, const std::vector<Entity>& entities);
-    void sendEntitiesToTFAndRViz(const std::string& rviz_topic_name, const std::vector<Entity>& entities);
-    void sendEntitiesToTF(const std::map<std::string, Object>& entities);
-    void sendEntitiesToRViz(const std::string& topic_name, const std::map<std::string, Object>& entities);
-    void sendEntitiesToTFAndRViz(const std::string& rviz_topic_name, const std::map<std::string, Object>& entities);
+    template<typename T> void sendEntitiesToTF(const std::vector<T*>& entities);
+    template<typename T> void sendEntitiesToRViz(const std::string& topic_name, const std::vector<T*>& entities);
+    template<typename T> void sendEntitiesToTFAndRViz(const std::string& rviz_topic_name, const std::vector<T*>& entities);
+    template<typename T> void sendEntitiesToTF(const std::map<std::string, T*>& entities);
+    template<typename T> void sendEntitiesToRViz(const std::string& topic_name, const std::map<std::string, T*>& entities);
+    template<typename T> void sendEntitiesToTFAndRViz(const std::string& rviz_topic_name, const std::map<std::string, T*>& entities);
     void sendImage(const std::string& topic_name, const b3CameraImageData& image);
 
   protected:
@@ -34,6 +34,127 @@ class ROSSender
     std::unordered_map<std::string, ros::Publisher> rviz_publishers_;
     std::unordered_map<std::string, image_transport::Publisher> image_publishers_;
 };
+
+template<typename T>
+void ROSSender::sendEntitiesToTF(const std::vector<T*>& entities)
+{
+    static_assert(std::is_base_of<Entity,T>::value, "T must be derived from Entity");
+    std::vector<geometry_msgs::TransformStamped> transforms;
+    for (const auto& entity : entities)
+    {
+        if (entity->isLocated())
+        {
+            transforms.push_back(entity->toTfTransform());
+        }
+    }
+    tf_broadcaster_.sendTransform(transforms);
+}
+
+template<typename T>
+void ROSSender::sendEntitiesToRViz(const std::string& topic_name, const std::vector<T*>& entities)
+{
+    static_assert(std::is_base_of<Entity,T>::value, "T must be derived from Entity");
+    if (rviz_publishers_.count(topic_name) == 0)
+    {
+        rviz_publishers_.insert(std::make_pair(topic_name, n_->advertise<visualization_msgs::MarkerArray>(topic_name, 1, false)));
+    }
+    ros::Publisher& pub = rviz_publishers_.at(topic_name);
+    visualization_msgs::MarkerArray markers;
+    size_t id = 0;
+    for (const auto& entity : entities)
+    {
+        if (entity->isLocated())
+        {
+            markers.markers.push_back(entity->toMarker(id++, 1.0, "marker"));
+        }
+    }
+    pub.publish(markers);
+}
+
+template<typename T>
+void ROSSender::sendEntitiesToTFAndRViz(const std::string& rviz_topic_name, const std::vector<T*>& entities)
+{
+    static_assert(std::is_base_of<Entity,T>::value, "T must be derived from Entity");
+    if (rviz_publishers_.count(rviz_topic_name) == 0)
+    {
+        rviz_publishers_.insert(std::make_pair(rviz_topic_name, n_->advertise<visualization_msgs::MarkerArray>(rviz_topic_name, 1, false)));
+    }
+    ros::Publisher& pub = rviz_publishers_.at(rviz_topic_name);
+    std::vector<geometry_msgs::TransformStamped> transforms;
+    visualization_msgs::MarkerArray markers;
+    size_t id = 0;
+    for (const auto& entity : entities)
+    {
+        if (entity->isLocated())
+        {
+            markers.markers.push_back(entity->toMarker(id++, 1.0, "marker"));
+            transforms.push_back(entity->toTfTransform());
+        }
+    }
+    pub.publish(markers);
+    tf_broadcaster_.sendTransform(transforms);
+}
+
+template<typename T>
+void ROSSender::sendEntitiesToTF(const std::map<std::string, T*>& entities)
+{
+    static_assert(std::is_base_of<Entity,T>::value, "T must be derived from Entity");
+    std::vector<geometry_msgs::TransformStamped> transforms;
+    for (const auto& entity : entities)
+    {
+        if (entity.second->isLocated())
+        {
+            transforms.push_back(entity.second->toTfTransform());
+        }
+    }
+    tf_broadcaster_.sendTransform(transforms);
+}
+
+template<typename T>
+void ROSSender::sendEntitiesToRViz(const std::string& topic_name, const std::map<std::string, T*>& entities)
+{
+    static_assert(std::is_base_of<Entity,T>::value, "T must be derived from Entity");
+    if (rviz_publishers_.count(topic_name) == 0)
+    {
+        rviz_publishers_.insert(std::make_pair(topic_name, n_->advertise<visualization_msgs::MarkerArray>(topic_name, 1, false)));
+    }
+    ros::Publisher& pub = rviz_publishers_.at(topic_name);
+    visualization_msgs::MarkerArray markers;
+    size_t id = 0;
+    for (const auto& entity : entities)
+    {
+        if (entity.second->isLocated())
+        {
+            markers.markers.push_back(entity.second->toMarker(id++, 1.0, "marker"));
+        }
+    }
+    pub.publish(markers);
+}
+
+template<typename T>
+void ROSSender::sendEntitiesToTFAndRViz(const std::string& rviz_topic_name, const std::map<std::string, T*>& entities)
+{
+    static_assert(std::is_base_of<Entity,T>::value, "T must be derived from Entity");
+    if (rviz_publishers_.count(rviz_topic_name) == 0)
+    {
+        rviz_publishers_.insert(std::make_pair(rviz_topic_name, n_->advertise<visualization_msgs::MarkerArray>(rviz_topic_name, 1, false)));
+    }
+    ros::Publisher& pub = rviz_publishers_.at(rviz_topic_name);
+    std::vector<geometry_msgs::TransformStamped> transforms;
+    visualization_msgs::MarkerArray markers;
+    size_t id = 0;
+    for (const auto& entity : entities)
+    {
+        if (entity.second->isLocated())
+        {
+            markers.markers.push_back(entity.second->toMarker(id++, 1.0, "marker"));
+            transforms.push_back(entity.second->toTfTransform());
+        }
+    }
+    pub.publish(markers);
+    tf_broadcaster_.sendTransform(transforms);
+}
+
 } // namespace owds
 
 #endif /* OWDS_ROSSENDER_H */
