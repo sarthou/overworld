99a1f0b63a3cd0a9bee6da83b175cb0f7ec4192e
Guillaume Sarthou <gsarthou@laas.fr>
2021-08-07 15:10:22 +0200

    [OntologeniusFactsPublisher] display facts

--- src/Facts/Publisher/OntologeniusFactsPublisher.cpp
+++ src/Facts/Publisher/OntologeniusFactsPublisher.cpp
@@ -14,11 +14,13 @@ OntologeniusFactsPublisher::OntologeniusFactsPublisher(ros::NodeHandle* n,
 
 void OntologeniusFactsPublisher::addToKb(const Fact& fact)
 {
+  std::cout << "[ADD] " << agent_name_ << " : " << fact.toString(" - ") << std::endl;
   onto_->feeder.addProperty(fact.getSubject(), fact.getPredicate(), fact.getObject());
 }
 
 void OntologeniusFactsPublisher::removeFromKb(const Fact& fact)
 {
+  std::cout << "[DELETE] " << agent_name_ << " : " << fact.toString(" - ") << std::endl;
   onto_->feeder.removeProperty(fact.getSubject(), fact.getPredicate(), fact.getObject());
 }
   
