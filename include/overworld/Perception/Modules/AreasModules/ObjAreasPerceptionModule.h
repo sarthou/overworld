c0560df55819c7315334c657410170ccb0600638
Guillaume Sarthou <gsarthou@laas.fr>
2023-05-07 17:29:55 +0200

    [ObjAreasPerceptionModule] new perception ;odules for areas

---
+++ include/overworld/Perception/Modules/AreasModules/ObjAreasPerceptionModule.h
@@ -0,0 +1,26 @@
+#ifndef OWDS_OBJAREASPERCEPTIONMODULE_H
+#define OWDS_OBJAREASPERCEPTIONMODULE_H
+
+#include "overworld/BasicTypes/Area.h"
+#include "overworld/Perception/Modules/PerceptionModuleBase.h"
+
+namespace owds {
+
+class ObjAreasPerceptionModule : public PerceptionModuleBase_<Area>
+{
+public:
+  ObjAreasPerceptionModule();
+
+  virtual void setParameter(const std::string& parameter_name, const std::string& parameter_value) override;
+  virtual bool closeInitialization() override;
+
+private:
+  std::string config_file_;
+
+  void addCircle(const std::string& id, const std::array<double, 3>& pose, double radius, double half_height, double hysteresis, const std::string& owner);
+  void addPolygon(const std::string& id, const std::string& polygon_path, double z_min, double z_max, double hysteresis, const std::string& owner);
+};
+
+} // namespace owds
+
+#endif // OWDS_OBJAREASPERCEPTIONMODULE_H
