a5cdac16a575fa9e0689d274affaf26e98b5a460
jevans-laas <jevans@laas.fr>
2024-06-19 20:49:30 +0200

    Updated GlmMath.h helper to please the compiler since we were accessing private members without going through glm::value_ptr

--- include/overworld/Helper/GlmMath.h
+++ include/overworld/Helper/GlmMath.h
@@ -1,11 +1,37 @@
 #ifndef OWDS_HELPER_GLMMATH_H
 #define OWDS_HELPER_GLMMATH_H
 
-#include "overworld/Helper/BitCast.h"
+#include <glm/gtc/type_ptr.hpp>
+#include <cstring>
 
-#define ToV3(x) owds::BitCast<glm::vec3>(x)
-#define FromV3(x) owds::BitCast<std::array<float, 3>>(x)
-#define ToM4(x) owds::BitCast<glm::mat4>(x)
-#define FromM4(x) owds::BitCast<std::array<float, 16>>(x)
+namespace owds {
+  inline glm::vec3 ToV3(const std::array<float, 3>& in)
+  {
+    glm::vec3 out;
+    std::memcpy(glm::value_ptr(out), std::addressof(in), sizeof(float) * 3);
+    return out;
+  }
+
+  inline glm::mat4 ToM4(const std::array<float, 16>& in)
+  {
+    glm::mat4 out;
+    std::memcpy(glm::value_ptr(out), std::addressof(in), sizeof(float) * 16);
+    return out;
+  }
+
+  inline std::array<float, 3> FromV3(const glm::vec3& in)
+  {
+    std::array<float, 3> out;
+    std::memcpy(std::addressof(out), glm::value_ptr(in), sizeof(float) * 3);
+    return out;
+  }
+
+  inline std::array<float, 16> FromM4(const glm::mat4& in)
+  {
+    std::array<float, 16> out;
+    std::memcpy(std::addressof(out), glm::value_ptr(in), sizeof(float) * 16);
+    return out;
+  }
+}
 
 #endif // OWDS_HELPER_GLMMATH_H
