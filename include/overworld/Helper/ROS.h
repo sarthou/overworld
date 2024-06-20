c78f14eb7010a4dbe12dd5af557eb1484b46057d
jevans-laas <jevans@laas.fr>
2024-06-20 16:53:50 +0200

    Added `owds::rosPkgPathToPath`

---
+++ include/overworld/Helper/ROS.h
@@ -0,0 +1,26 @@
+#ifndef OWDS_HELPER_ROS_H
+#define OWDS_HELPER_ROS_H
+
+namespace owds {
+  inline std::string rosPkgPathToPath(const std::string& filename)
+  {
+    const auto url_handler = filename.substr(0, 10);
+
+    assert(url_handler == "package://");
+
+    const auto total_path = filename.substr(10);
+
+    auto parent_path = std::filesystem::path(total_path);
+    while(parent_path.has_parent_path())
+    {
+      parent_path = parent_path.parent_path();
+    }
+
+    const auto assets_path = compat::owds_ros::getShareDirectory(parent_path.string());
+    const auto relative_path = total_path.substr(parent_path.string().size() + 1);
+
+    return assets_path + "/" + relative_path;
+  }
+} // namespace owds
+
+#endif // OWDS_HELPER_ROS_H
