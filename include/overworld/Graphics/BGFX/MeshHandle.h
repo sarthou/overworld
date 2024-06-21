fa1bab68c651e30de35321dee2b36c195f93e2f0
jevans-laas <jevans@laas.fr>
2024-06-21 11:36:51 +0200

    Added color information to `owds::bgfx::MeshHandle`

--- include/overworld/Graphics/BGFX/MeshHandle.h
+++ include/overworld/Graphics/BGFX/MeshHandle.h
@@ -2,11 +2,13 @@
 #define OWDS_GRAPHICS_BGFX_MESH_H
 
 #include "overworld/Graphics/BGFX/API.h"
+#include "overworld/Graphics/Base/Color.h"
 
 namespace owds::bgfx {
   class MeshHandle
   {
   public:
+    ::owds::Color color_rgba_{};
     ::bgfx::TextureHandle tex_{};
     ::bgfx::VertexBufferHandle vbh_{};
     ::bgfx::IndexBufferHandle ibh_{};
