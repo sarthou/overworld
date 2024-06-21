3846f8a07c627065e78c862017e89219cc3e4b72
jevans-laas <jevans@laas.fr>
2024-06-21 11:33:14 +0200

    Added material inforkation in `owds::Robot`

--- include/overworld/Physics/Base/Robot.h
+++ include/overworld/Physics/Base/Robot.h
@@ -3,15 +3,21 @@
 
 #include <string>
 #include <unordered_map>
+#include "overworld/Graphics/Base/Material.h"
 
 namespace owds {
   class Actor;
+  class Material;
 
   class Robot
   {
   public:
+    Robot();
+    ~Robot() noexcept;
+
     std::string name_;
-    std::unordered_map<std::string, Actor*> links_;
+    std::unordered_map<std::string, owds::Actor*> links_;
+    std::unordered_map<std::string, owds::Material> materials_;
   };
 } // namespace owds
 
